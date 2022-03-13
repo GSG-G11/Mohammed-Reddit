b692446c47491c8dfeb91b4c7956d279a5d9dccc
Mohammed-Hamada <mohammed3rbio@gmail.com>
2022-03-13 23:49:11 +0200

    update query to select all data by email Relates #22

--- server/database/queries/getUserByEmail.js
+++ server/database/queries/getUserByEmail.js
@@ -1,3 +1,3 @@
 const connection = require('../config/connection');
 
-module.exports = (email) => connection.query('SELECT username, email FROM users WHERE email = $1;', [email]);
+module.exports = (email) => connection.query('SELECT * FROM users WHERE email = $1;', [email]);
