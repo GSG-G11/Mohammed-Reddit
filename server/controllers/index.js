092e4e8aa513976ee4c94de35d249643aa103cd0
Mohammed-Hamada <mohammed3rbio@gmail.com>
2022-03-13 23:44:03 +0200

    export loginLogicHandler function Relates #22

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,7 +1,9 @@
-const customizedError = require('./customizedError');
+const loginLogicHandler = require('./loginLogicHandler');
 const loginPageHandler = require('./loginPageHandler');
+const customizedError = require('../auth/customizedError');
 
 module.exports = {
   loginPageHandler,
+  loginLogicHandler,
   customizedError,
 };
