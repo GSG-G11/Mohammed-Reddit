26742146711778c53c86790a57072f815a60ba25
Mohammed-Hamada <mohammed3rbio@gmail.com>
2022-03-13 15:03:15 +0200

    Create function that hashing a password passed to it using Relates #16

--- server/utils/index.js
+++ server/utils/index.js
@@ -1,7 +1,9 @@
 const loginSchema = require('./validation/loginSchema');
 const signupSchema = require('./validation/signupSchema');
+const hashingPassword = require('./hashingPassword');
 
 module.exports = {
   loginSchema,
   signupSchema,
+  hashingPassword,
 };
