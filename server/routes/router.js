9619434c640dd01a4fd78c327141957c569b3509
Mohammed-Hamada <mohammed3rbio@gmail.com>
2022-03-14 01:15:30 +0200

    handle client errors and server error Relates #24

--- server/routes/router.js
+++ server/routes/router.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
 const { verifyAuthentication } = require('../auth');
-const { loginPageHandler, loginLogicHandler } = require('../controllers');
+const { loginPageHandler, loginLogicHandler, pageNotFoundError, serverError} = require('../controllers');
 
 router.route('/login').get(loginPageHandler).post(loginLogicHandler)
+
+
+router.use(pageNotFoundError);
+router.use(serverError)
 
 module.exports = router;
