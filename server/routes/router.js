ccd5e92ea6d1cfba214998ba675c1f02655f22c1
Mohammed-Hamada <mohammed3rbio@gmail.com>
2022-03-14 00:19:35 +0200

    handle /login endpoint with 'POST' method Relates #22

--- server/routes/router.js
+++ server/routes/router.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
-const { loginPageHandler } = require('../controllers');
+const { verifyAuthentication } = require('../auth');
+const { loginPageHandler, loginLogicHandler } = require('../controllers');
 
 router.get('/login', loginPageHandler);
+router.post('/login', loginLogicHandler);
 
 module.exports = router;
