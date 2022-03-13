ab264f325bf5c189682be5becab389d04790dff8
Mohammed-Hamada <mohammed3rbio@gmail.com>
2022-03-14 00:18:13 +0200

    Edit verifyAuthentication.js Function Relates #22

--- server/auth/verifyAuthentication.js
+++ server/auth/verifyAuthentication.js
@@ -1,7 +1,8 @@
 const { verify } = require('jsonwebtoken');
 const customizedError = require('./customizedError');
 
-const verifyAuthentication = (token) => new Promise((resolve, reject) => {
+const verifyAuthentication = (req, res, next) => new Promise((resolve, reject) => {
+  const token = req.cookies.Access_Token;
   verify(token, process.env.SECRET, (err, decoded) => {
     if (err) {
       reject(customizedError({ errorMessage: 'User not authenticated', status: 401 }));
