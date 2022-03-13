4d4842fbd18b64637fa48cbb2a8db72b045ac71e
Mohammed-Hamada <mohammed3rbio@gmail.com>
2022-03-13 20:04:07 +0200

    create getUserByEmail query Relates #22

--- server/database/config/build/init.sql
+++ server/database/config/build/init.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS users,comments,posts CASCADE;
 CREATE TABLE users(
   id SERIAL PRIMARY KEY,
   username VARCHAR(50) NOT NULL,
-  email VARCHAR(50) NOT NULL,
+  email VARCHAR(50) NOT NULL UNIQUE,
   password VARCHAR(100) NOT NULL
 );
 CREATE TABLE posts(
