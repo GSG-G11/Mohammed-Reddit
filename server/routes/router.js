e3e0ed85ea35dc504e72b58907719422a150c41b
Mohammed-Hamada <mohammed3rbio@gmail.com>
2022-03-14 12:18:36 +0200

    handle '/register' endpoint with GET method Relates #29

--- server/routes/router.js
+++ server/routes/router.js
@@ -1,11 +1,17 @@
 const router = require('express').Router();
 const { verifyAuthentication } = require('../auth');
-const { loginPageHandler, loginLogicHandler, pageNotFoundError, serverError} = require('../controllers');
+const {
+  loginPageHandler,
+  loginLogicHandler,
+  pageNotFoundError,
+  serverError,
+  registerPageHandler,
+} = require('../controllers');
 
-router.route('/login').get(loginPageHandler).post(loginLogicHandler)
-
+router.route('/login').get(loginPageHandler).post(loginLogicHandler);
+router.route('/register').get(registerPageHandler);
 
 router.use(pageNotFoundError);
-router.use(serverError)
+router.use(serverError);
 
 module.exports = router;
