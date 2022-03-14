c43546b88a45f67486d1cffc3a635c43559bcef4
Mohammed-Hamada <mohammed3rbio@gmail.com>
2022-03-14 17:11:25 +0200

    Check Authentication for home page Relates #31

--- server/routes/router.js
+++ server/routes/router.js
@@ -1,16 +1,19 @@
 const router = require('express').Router();
-const { verifyAuthentication } = require('../auth');
+const { verifyAuthentication, checkAuthentication } = require('../auth');
 const {
   loginPageHandler,
   loginLogicHandler,
+  registerPageHandler,
+  registerLogicHandler,
+  homePageHandler,
   pageNotFoundError,
   serverError,
-  registerPageHandler,
-  registerLogicHandler
 } = require('../controllers');
 
 router.route('/login').get(loginPageHandler).post(loginLogicHandler);
 router.route('/register').get(registerPageHandler).post(registerLogicHandler);
+
+router.get('/home', checkAuthentication, homePageHandler);
 
 router.use(pageNotFoundError);
 router.use(serverError);
