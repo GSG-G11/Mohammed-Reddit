0c475c8476edc89acea3d43f5753b0c3e8c1fb1a
Mohammed-Hamada <mohammed3rbio@gmail.com>
2022-03-14 13:56:37 +0200

    Create 'registerLogicHandler.js' function that handle data from register form Final Commit >_< Relates #29

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -2,10 +2,12 @@ const loginPageHandler = require('./loginPageHandler');
 const registerPageHandler = require('./registerPageHandler');
 const loginLogicHandler = require('./loginLogicHandler');
 const { pageNotFoundError, serverError } = require('./errors');
+const registerLogicHandler = require('./registerLogicHandler');
 module.exports = {
   loginPageHandler,
   loginLogicHandler,
   pageNotFoundError,
   serverError,
-  registerPageHandler
+  registerPageHandler,
+  registerLogicHandler
 };
---
+++ server/controllers/registerLogicHandler.js
@@ -0,0 +1,57 @@
+const { customizedError, signAuthentication } = require('../auth');
+const { addUser } = require('../database/queries');
+const { signupSchema, hashingPassword } = require('../utils');
+
+const registerLogicHandler = (req, res, next) => {
+  let { username, email, password } = req.body;
+  let id;
+  signupSchema
+    .validateAsync(req.body)
+    .then(() => hashingPassword(password))
+    .then((hashedPassword) => {
+      return addUser(username, email, hashedPassword);
+    })
+    .then((data) => {
+      if (!data.rows.length) {
+        throw customizedError({
+          errorMessage: 'This email already exists, try again',
+          status: 400,
+        });
+      } else {
+        id = data.rows[0].id;
+        username = data.rows[0].username;
+        return signAuthentication({ id });
+      }
+    })
+    .then((token) => {
+      if (token) {
+        res
+          .cookie('Access_Token', token, {
+            httpOnly: true,
+            maxAge: 900000000,
+          })
+          .send({ username, message: 'Successfully Registered' });
+      }
+    })
+    .catch((err) => {
+      if (
+        err.message ===
+        'duplicate key value violates unique constraint "users_email_key"'
+      ) {
+        next(
+          customizedError({
+            errorMessage: 'This email already exists, try again',
+            status: 400,
+          })
+        );
+      } else if (err.details) {
+        next(
+          customizedError({ errorMessage: err.details[0].message, status: 400 })
+        );
+      } else {
+        next(err);
+      }
+    });
+};
+
+module.exports = registerLogicHandler;
