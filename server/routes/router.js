a97cce26d32337ac9afaf7ce2e2bd3fc1c7caf0f
Mohammed-Hamada <mohammed3rbio@gmail.com>
2022-03-14 13:54:10 +0200

    handle '/register' endpoint with POST method Relates #29

--- server/routes/router.js
+++ server/routes/router.js
@@ -6,10 +6,11 @@ const {
   pageNotFoundError,
   serverError,
   registerPageHandler,
+  registerLogicHandler
 } = require('../controllers');
 
 router.route('/login').get(loginPageHandler).post(loginLogicHandler);
-router.route('/register').get(registerPageHandler);
+router.route('/register').get(registerPageHandler).post(registerLogicHandler);
 
 router.use(pageNotFoundError);
 router.use(serverError);
