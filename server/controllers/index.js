d2d46670bb755691c4f2f6768c169ddea4ef7242
Mohammed-Hamada <mohammed3rbio@gmail.com>
2022-03-14 17:10:31 +0200

    Handle home page Relates #31

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -3,11 +3,13 @@ const registerPageHandler = require('./registerPageHandler');
 const loginLogicHandler = require('./loginLogicHandler');
 const { pageNotFoundError, serverError } = require('./errors');
 const registerLogicHandler = require('./registerLogicHandler');
+const homePageHandler = require('./homePageHandler');
 module.exports = {
   loginPageHandler,
   loginLogicHandler,
   pageNotFoundError,
   serverError,
   registerPageHandler,
-  registerLogicHandler
+  registerLogicHandler,
+  homePageHandler,
 };
