d09922a836fe6550dd2d79b8eb095e42a500cd4f
Mohammed-Hamada <mohammed3rbio@gmail.com>
2022-03-16 00:17:01 +0200

    Create post handlers Relates #34

--- server/controllers/postsHandlers.js
+++ server/controllers/postsHandlers.js
@@ -1,19 +1,93 @@
-const { addPost } = require('../database/queries');
+const { customizedError } = require('../auth');
+const {
+  addPost,
+  upRating,
+  downRating,
+  getAllPosts,
+  deletePost,
+} = require('../database/queries');
 
 const addPostHandler = (req, res, next) => {
-  const { id } = req.query;
-  new Date().getHours
-  let date = `${new Date().getHours()}:${new Date().getMinutes()} ${new Date().getFullYear()}-${
-    new Date().getMonth() + 1
-  }-${new Date().getDate()}`;
+  const user_id = req.user_id;
+  const time = new Date();
+  let date = `${time.getHours()}:${time.getMinutes()} ${time.getFullYear()}-${
+    time.getMonth() + 1
+  }-${time.getDate()}`;
   const { title, content } = req.body;
-  addPost(id, title, content, date)
+  addPost(user_id, title, content, date)
     .then((data) => {
       res.status(200).json({ message: 'success', data: data.rows[0] });
     })
     .catch(next);
 };
 
+const upRatingPost = (req, res, next) => {
+  const postId = req.params.post_id;
+  if (!req.params.post_id) {
+    next(
+      customizedError({ errorMessage: 'Failed to add rating', status: 400 })
+    );
+  } else {
+    upRating(postId)
+      .then((data) => {
+        res.status(200).json({ message: 'success', data: data.rows[0] });
+      })
+      .catch(next);
+  }
+};
+
+const downRatingPost = (req, res, next) => {
+  const postId = req.params.post_id;
+  if (!req.params.post_id) {
+    next(
+      customizedError({ errorMessage: 'Failed to add rating', status: 400 })
+    );
+  } else {
+    downRating(postId)
+      .then((data) => {
+        res.status(200).json({ message: 'success', data: data.rows[0] });
+      })
+      .catch(next);
+  }
+};
+
+const allPostsHandler = (req, res, next) => {
+  getAllPosts()
+    .then((data) => {
+      if (!data.rowCount) {
+        next(
+          customizedError({
+            errorMessage: 'No posts yet, add new post to see it.',
+            status: 204,
+          })
+        );
+      }
+      res.status(200).json({ message: 'success', data: data.rows });
+    })
+    .catch(next);
+};
+
+const deletePostHandler = (req, res, next) => {
+  const { post_id } = req.params;
+  deletePost(post_id)
+    .then((data) => {
+      if (!data.rowCount) {
+        next(
+          customizedError({
+            errorMessage: 'No posts to delete.',
+            status: 400,
+          })
+        );
+      } else {
+        res.status(200).json({ message: 'success', data: data.rows });
+      }
+    })
+    .catch(next);
+};
 module.exports = {
   addPostHandler,
+  upRatingPost,
+  downRatingPost,
+  allPostsHandler,
+  deletePostHandler,
 };
