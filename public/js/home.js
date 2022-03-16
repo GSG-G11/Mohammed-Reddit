090a009f48bd714a2935c0f5e025e2a26c200f8a
Mohammed-Hamada <mohammed3rbio@gmail.com>
2022-03-16 16:14:00 +0200

    handle posts Relates #34

--- public/js/home.js
+++ public/js/home.js
@@ -3,10 +3,84 @@ const postTitle = document.getElementById('post-title');
 const postContent = document.getElementById('post-content');
 const addPostForm = document.getElementById('add-post-form');
 const postsContainer = document.querySelector('.posts-container');
-
 let user_id;
 let user_name;
-window.addEventListener('load', (e) => {
+
+const createElement = (tagName, className) => {
+  const element = document.createElement(tagName);
+  element.className = className;
+  return element;
+};
+const createPost = (parent, data, username, postId) => {
+  const post = createElement('div', 'post');
+  const postSideBar = createElement('div', 'post-side-bar');
+  const upArrowSpan = createElement('span', 'up arrow');
+  const upArrowIcon = createElement('i', 'fa fa-arrow-up');
+  const ratingSpan = createElement('span', 'rating');
+  const downArrowSpan = createElement('span', 'down arrow');
+  const downArrowIcon = createElement('i', 'fa fa-arrow-down');
+  const postHeader = createElement('div', 'post-header');
+  const postGeneral = createElement('div', 'post-general');
+  const dotsIcon = createElement('i', 'icon fa fa-ellipsis-h dots');
+  const save = createElement('p', 'parag save');
+  const saveIcon = createElement('i', 'icon fa fa-angle-up');
+  const share = createElement('p', 'parag share');
+  const shareIcon = createElement('i', 'icon fa fa-share');
+  const comment = createElement('p', 'parag comment');
+  const commentIcon = createElement('i', 'icon far fa-comment-alt');
+  const userImageLink = createElement('a', 'user-image-link');
+  const userImage = createElement('img', 'user-image');
+  const postFooter = createElement('div', 'post-footer');
+  const postParagraph = createElement('div', 'post-paragraph');
+  const postTitle = createElement('div', 'post-title');
+  const postContent = createElement('div', 'post-content');
+  const userName = createElement('p', 'user-name');
+  const postInformation = createElement('p', 'post-information');
+
+  userImage.alt = 'User image';
+  userImage.style.width = '20px';
+  userImage.src = '../assets/user.png';
+
+  post.appendChild(postSideBar);
+  post.appendChild(postGeneral);
+  postSideBar.appendChild(upArrowSpan);
+  upArrowSpan.appendChild(upArrowIcon);
+  postSideBar.appendChild(ratingSpan);
+  postSideBar.appendChild(downArrowSpan);
+  downArrowSpan.appendChild(downArrowIcon);
+  postGeneral.appendChild(postHeader);
+  postGeneral.appendChild(postContent);
+  postGeneral.appendChild(postFooter);
+  postHeader.appendChild(userImageLink);
+  postHeader.appendChild(userName);
+  postHeader.appendChild(postInformation);
+  userImageLink.appendChild(userImage);
+  postContent.appendChild(postTitle);
+  postContent.appendChild(postParagraph);
+  postFooter.appendChild(commentIcon);
+  postFooter.appendChild(comment);
+  postFooter.appendChild(shareIcon);
+  postFooter.appendChild(share);
+  postFooter.appendChild(saveIcon);
+  postFooter.appendChild(save);
+  postFooter.appendChild(dotsIcon);
+
+  postTitle.textContent = data.title;
+  postParagraph.textContent = data.content;
+  postInformation.textContent = `Posted by ${user_name} at ${data.date}`;
+  ratingSpan.textContent = data.vote === null ? '0' : data.vote;
+  userName.textContent = username;
+  comment.textContent = 'Comment';
+  share.textContent = 'Share';
+  save.textContent = 'Save';
+  post.id = postId;
+  upArrowSpan.setAttribute('onclick', 'upVote(this)');
+  downArrowSpan.setAttribute('onclick', 'downVote(this)');
+  parent.appendChild(post);
+  return post;
+};
+
+document.addEventListener('DOMContentLoaded', (e) => {
   e.preventDefault();
   fetch('/home', {
     headers: {
@@ -16,15 +90,56 @@ window.addEventListener('load', (e) => {
   })
     .then((response) => response.json())
     .then((result) => {
-      user_id = result.user_data.id;
-      user_name = result.user_data.username;
-      userProfile.textContent = result.user_data.username;
-      userProfile.href = `profile?id=${result.user_data.id}`;
+      if (!result.data) {
+        user_id = result.user_data.id;
+        user_name = result.user_data.username;
+        userProfile.textContent = result.user_data.username;
+      } else {
+        window.location.href = '/login';
+      }
     })
     .catch((err) => {
-      window.location.href = '/home';
+      console.log(err);
+    });
+  fetch('/post')
+    .then((response) => response.json())
+    .then((result) => {
+      const posts = [...result.data];
+      postsContainer.textContent = '';
+      posts.forEach((element) => {
+        createPost(postsContainer, element, element.username, element.id);
+      });
+    })
+    .catch((err) => {
+      // postsContainer.textContent = 'No posts yet';
     });
 });
+const upVote = (el) => {
+  fetch(`/post/up/${el.parentElement.parentElement.id}`, {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    method: 'POST',
+  })
+    .then((response) => response.json())
+    .then((result) => {
+      el.parentElement.children[1].textContent = result.data.vote;
+    })
+    .catch();
+};
+const downVote = (el) => {
+  fetch(`/post/down/${el.parentElement.parentElement.id}`, {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    method: 'POST',
+  })
+    .then((response) => response.json())
+    .then((result) => {
+      el.parentElement.children[1].textContent = result.data.vote;
+    })
+    .catch();
+};
 
 addPostForm.addEventListener('submit', (e) => {
   e.preventDefault();
@@ -32,7 +147,7 @@ addPostForm.addEventListener('submit', (e) => {
     title: postTitle.value,
     content: postContent.value,
   };
-  fetch(`/post?id=${user_id}`, {
+  fetch(`/post`, {
     headers: {
       'Content-Type': 'application/json',
     },
@@ -41,89 +156,9 @@ addPostForm.addEventListener('submit', (e) => {
   })
     .then((response) => response.json())
     .then((result) => {
-      console.log(result.data);
-      const post = document.createElement('div');
-      const postSideBar = document.createElement('div');
-      const upArrowSpan = document.createElement('span');
-      const upArrowIcon = document.createElement('i');
-      const ratingSpan = document.createElement('span');
-      const downArrowSpan = document.createElement('span');
-      const downArrowIcon = document.createElement('i');
-      const postHeader = document.createElement('div');
-      const postGeneral = document.createElement('div');
-      const dotsIcon = document.createElement('i');
-      const save = document.createElement('p');
-      const saveIcon = document.createElement('i');
-      const share = document.createElement('p');
-      const shareIcon = document.createElement('i');
-      const comment = document.createElement('p');
-      const userImageLink = document.createElement('a');
-      const commentIcon = document.createElement('i');
-      const userImage = document.createElement('img');
-      const postFooter = document.createElement('div');
-      const postParagraph = document.createElement('div');
-      const postContent = document.createElement('div');
-      const userName = document.createElement('p');
-      const postInformation = document.createElement('p');
-
-      post.className = 'post';
-      postSideBar.className = 'post-side-bar';
-      upArrowSpan.className = 'up arrow';
-      upArrowIcon.className = 'fa fa-arrow-up';
-      ratingSpan.className = 'rating';
-      downArrowSpan.className = 'down arrow';
-      downArrowIcon.className = 'fa fa-arrow-down';
-      postGeneral.className = 'post-general';
-      postHeader.className = 'post-header';
-      userImageLink.className = 'user-image-link';
-      userImage.className = 'user-image';
-      userImage.alt = 'User image';
-      userImage.style.width = '20px';
-      userImage.src = '../assets/user.png'
-      userName.className = 'user-name';
-      postInformation.className = 'post-information';
-      postContent.className = 'post-content';
-      postParagraph.className = 'post-paragraph';
-      postFooter.className = 'post-footer';
-      commentIcon.className = 'icon far fa-comment-alt';
-      comment.className = 'parag comment';
-      shareIcon.className = 'icon fa fa-share';
-      share.className = 'parag share';
-      saveIcon.className = 'icon fa fa-angle-up';
-      save.className = 'parag save';
-      dotsIcon.className = 'icon fa fa-ellipsis-h dots';
-
-      post.appendChild(postSideBar);
-      post.appendChild(postGeneral);
-      postSideBar.appendChild(upArrowSpan);
-      upArrowSpan.appendChild(upArrowIcon);
-      postSideBar.appendChild(ratingSpan);
-      postSideBar.appendChild(downArrowSpan);
-      downArrowSpan.appendChild(downArrowIcon);
-      postGeneral.appendChild(postHeader);
-      postGeneral.appendChild(postContent);
-      postGeneral.appendChild(postFooter);
-      postHeader.appendChild(userImageLink);
-      postHeader.appendChild(userName);
-      postHeader.appendChild(postInformation);
-      userImageLink.appendChild(userImage);
-      postContent.appendChild(postParagraph);
-      postFooter.appendChild(commentIcon);
-      postFooter.appendChild(comment);
-      postFooter.appendChild(shareIcon);
-      postFooter.appendChild(share);
-      postFooter.appendChild(saveIcon);
-      postFooter.appendChild(save);
-      postFooter.appendChild(dotsIcon);
-
-      postParagraph.textContent = result.data.content;
-      postInformation.textContent = `Posted by ${user_name} at ${result.data.date}`;
-      ratingSpan.textContent =
-        result.data.vote === null ? '0' : result.data.vote;
-      userName.textContent = user_name;
-      postsContainer.appendChild(post);
-      comment.textContent = 'Comment';
-      share.textContent = 'Share';
-      save.textContent = 'Save';
+      createPost(postsContainer, result.data, user_name, result.data.id);
+    })
+    .catch((err) => {
+      window.location.href = '/login';
     });
 });
